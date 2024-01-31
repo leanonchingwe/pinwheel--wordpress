500daa0796a74a0ca653bec58e6b361bafb4955a
leanonchingwe <66792863+leanonchingwe@users.noreply.github.com>
2024-01-31 15:07:25 +0200

    main

--- src/Blog/singleservice.js
+++ src/Blog/singleservice.js
@@ -19,8 +19,187 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Create a card for the blog post
         const card = document.createElement("div");
-        card.classList.add("col-md-4");
+        card.classList.add("integration-single-container");
         card.innerHTML = `
+
+        <div class="px-4 text-center">
+        <img
+          class="mx-auto"
+          src="images/icons/webflow-colored.svg"
+          alt=""
+        />
+        <h1 class="mt-6">${blogPost.title}</h1>
+        <p class="mt-6">
+          We are looking for a personal financial planning pro Certified
+          Financial Planner preferred)<br />
+          who will lead our client advising efforts. You will be a fiduciary
+          who works
+        </p>
+        <a class="btn btn-primary mt-8 px-10" href="#">Integrate Webflow</a>
+      </div>
+      <div class="my-12 border-y border-border py-3">
+        <ul class="tab-nav filter-list justify-center border-b-0" data-tab-nav>
+          <li>
+            <a class="filter-btn active btn btn-sm" data-tab="0" href="#"
+              >About Setup</a
+            >
+          </li>
+          <li>
+            <a
+              class="filter-btn btn btn-sm inline-block border border-[#A9A9A9] text-dark" data-tab="1"
+              href="#"
+              >How to setup</a
+            >
+          </li>
+          <li>
+            <a
+              class="filter-btn btn btn-sm inline-block border border-[#A9A9A9] text-dark" data-tab="2"
+              href="#"
+              >Support</a
+            >
+          </li>
+        </ul>
+      </div>
+      <div class="integrations-single-content px-4 tab-content" data-tab-content >
+        <div class="tab-content-panel" data-tab-panel="0">
+          <h4>How to connect your store with Webflow</h4>
+          <p>
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+            posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam
+            vel, ullamcorper sit amet ligula. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Vivamus magna justo, lacinia eget
+            consectetur sed, convallis at tellus. Nulla quis lorem ut libero
+            malesuada feugiat. Mauris blandit aliquet elit, eget tincidunt
+            nibh pulvinar a. Vivamus suscipit tortor eget felis porttitor
+            volutpat. Cras ultricies ligula sed magna dictum porta. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit
+            aliquet elit, eget tincidunt nibh pulvinar a. Nulla porttitor
+            accumsan tincidunt. Nulla porttitor accumsan tincidunt. Sed
+            porttitor lectus nibh. Donec sollicitudin molestie malesuada.
+            Donec sollicitudin
+          </p>
+          <ul>
+            <li>
+              Enhance or improve User experience, our Site, or our Service.
+            </li>
+            <li>Process transactions.</li>
+            <li>Send emails about our Site or respond to inquiries.</li>
+            <li>
+              Send emails and updates about Conclude, including news and
+              requests for agreement to amended legal documents such
+            </li>
+            <li>
+              Perform any other function that we believe in good is necessary
+              to protect the or proper functioning of our Site or Service.
+            </li>
+          </ul>
+          <h4>Why opening Webflow sales channels?</h4>
+          <p>
+            feugiat. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar
+            a. Vivamus suscipit tortor eget felis porttitor volutpat. Cras
+            ultricies ligula sed magna dictum porta. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit. Mauris blandit aliquet elit,
+            eget tincidunt nibh pulvinar a. Nulla porttitor accumsan
+            tincidunt. Nulla porttitor accumsan tincidunt. Sed porttitor
+            lectus nibh. Donec sollicitudin molestie malesuada. Donec
+            sollicitudin
+          </p>
+        </div>
+        <div class="tab-content-panel" data-tab-panel="1">
+          <h4>How to connect your store with Webflow 1</h4>
+          <p>
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+            posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam
+            vel, ullamcorper sit amet ligula. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Vivamus magna justo, lacinia eget
+            consectetur sed, convallis at tellus. Nulla quis lorem ut libero
+            malesuada feugiat. Mauris blandit aliquet elit, eget tincidunt
+            nibh pulvinar a. Vivamus suscipit tortor eget felis porttitor
+            volutpat. Cras ultricies ligula sed magna dictum porta. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit
+            aliquet elit, eget tincidunt nibh pulvinar a. Nulla porttitor
+            accumsan tincidunt. Nulla porttitor accumsan tincidunt. Sed
+            porttitor lectus nibh. Donec sollicitudin molestie malesuada.
+            Donec sollicitudin
+          </p>
+          <ul>
+            <li>
+              Enhance or improve User experience, our Site, or our Service.
+            </li>
+            <li>Process transactions.</li>
+            <li>Send emails about our Site or respond to inquiries.</li>
+            <li>
+              Send emails and updates about Conclude, including news and
+              requests for agreement to amended legal documents such
+            </li>
+            <li>
+              Perform any other function that we believe in good is necessary
+              to protect the or proper functioning of our Site or Service.
+            </li>
+          </ul>
+          <h4>Why opening Webflow sales channels?</h4>
+          <p>
+            feugiat. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar
+            a. Vivamus suscipit tortor eget felis porttitor volutpat. Cras
+            ultricies ligula sed magna dictum porta. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit. Mauris blandit aliquet elit,
+            eget tincidunt nibh pulvinar a. Nulla porttitor accumsan
+            tincidunt. Nulla porttitor accumsan tincidunt. Sed porttitor
+            lectus nibh. Donec sollicitudin molestie malesuada. Donec
+            sollicitudin
+          </p>
+        </div>
+        <div class="tab-content-panel" data-tab-panel="2">
+          <h4>How to connect your store with Webflow 2</h4>
+          <p>
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+            posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam
+            vel, ullamcorper sit amet ligula. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Vivamus magna justo, lacinia eget
+            consectetur sed, convallis at tellus. Nulla quis lorem ut libero
+            malesuada feugiat. Mauris blandit aliquet elit, eget tincidunt
+            nibh pulvinar a. Vivamus suscipit tortor eget felis porttitor
+            volutpat. Cras ultricies ligula sed magna dictum porta. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit
+            aliquet elit, eget tincidunt nibh pulvinar a. Nulla porttitor
+            accumsan tincidunt. Nulla porttitor accumsan tincidunt. Sed
+            porttitor lectus nibh. Donec sollicitudin molestie malesuada.
+            Donec sollicitudin
+          </p>
+          <ul>
+            <li>
+              Enhance or improve User experience, our Site, or our Service.
+            </li>
+            <li>Process transactions.</li>
+            <li>Send emails about our Site or respond to inquiries.</li>
+            <li>
+              Send emails and updates about Conclude, including news and
+              requests for agreement to amended legal documents such
+            </li>
+            <li>
+              Perform any other function that we believe in good is necessary
+              to protect the or proper functioning of our Site or Service.
+            </li>
+          </ul>
+          <h4>Why opening Webflow sales channels?</h4>
+          <p>
+            feugiat. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar
+            a. Vivamus suscipit tortor eget felis porttitor volutpat. Cras
+            ultricies ligula sed magna dictum porta. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit. Mauris blandit aliquet elit,
+            eget tincidunt nibh pulvinar a. Nulla porttitor accumsan
+            tincidunt. Nulla porttitor accumsan tincidunt. Sed porttitor
+            lectus nibh. Donec sollicitudin molestie malesuada. Donec
+            sollicitudin
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div
+
+
+
             <div class="card mb-4">
                 <img src="assets/images/${blogPost.image}" class="card-img-top" alt="Blog Image">
                 <div class="card-body">
@@ -38,3 +217,4 @@ document.addEventListener("DOMContentLoaded", function () {
     
      fetchAllBlogPosts();
 });
+
